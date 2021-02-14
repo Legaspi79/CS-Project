1c163e7482cd44b2661e2b62fd40dc64a9199ba1
slashkoopa <49096720+slashkoopa@users.noreply.github.com>
2021-02-14 19:28:39 +0800

    Grab and Throw (not working fully)
    when throwing the game thinks that the present is still intact or within grabbing radius of the character..

---
+++ Game project/Assets/aimove.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection.Emit;
+using System.Security.Cryptography;
+using System.Timers;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aimove : MonoBehaviour
+{
+    public float speed;
+    public bool MoveRight;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(MoveRight){
+            transform.Translate(1 * Time.deltaTime * speed, 0,0);
+            transform.localScale = new Vector2 (1,1);
+        }
+        else{
+            transform.Translate(-1 * Time.deltaTime * speed, 0,0);
+            transform.localScale = new Vector2 (-1,1);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        if(trig.gameObject.CompareTag("turn")){
+            if(MoveRight){
+                MoveRight = false;
+            }
+            else{
+                MoveRight = true;
+            }
+        }
+    }
+}
---
+++ Game project/Assets/grabandthrow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grabandthrow : MonoBehaviour
+{
+    public Transform grabDetect;
+    public Transform boxHolder;
+    public float rayDist;
+    public float throwforce;
+    public bool grabbed=false;
+    public LayerMask notgrabbed;
+    public bool thrown;
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale, rayDist); 
+        if(grabbed){
+          grabCheck.collider.gameObject.transform.parent = boxHolder;
+          grabCheck.collider.gameObject.transform.position = boxHolder.position;
+          grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+        }
+        if(Input.GetMouseButtonDown(0)){
+             print("pressed");
+            if(grabbed){
+                print("throwing");
+                grabbed = false;
+                if(grabCheck.collider.gameObject.GetComponent<Rigidbody2D>() !=null){
+                    grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x,1)*throwforce;
+                   
+                    
+                    grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                }
+            }
+            else if(!grabbed){
+                print("grabbing");
+                if(grabCheck.collider != null && grabCheck.collider.tag == "box"){
+                    grabbed = true;
+                    
+                }
+            }    
+        }
+        
+    }
+}
