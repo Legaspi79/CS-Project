53fa76c17b561aef2378ae156357bac7271e4559
Gerald Cadiao <frostvind43@gmail.com>
2021-02-14 13:26:28 +0800

    Added back button functionality to main settings and updated unity version to 2019.4.19f1

---
+++ Game project/Assets/MainSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSettings : MonoBehaviour
+{
+    public void BackToMain() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
